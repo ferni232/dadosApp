22ff3631d7400365de94ea728eb87f0d8b823a3a
Ivan Krupik <krupikivan@gmail.com>
2021-03-04 23:59:49 -0300

    Dice app

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,65 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
           backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
+          appBar: AppBar(
+            title: Text('Dados'),
+            backgroundColor: Colors.red,
+          ),
+          body: PaginaDados()),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class PaginaDados extends StatefulWidget {
+  @override
+  _PaginaDadosState createState() => _PaginaDadosState();
+}
+
+class _PaginaDadosState extends State<PaginaDados> {
+  int numeroDado = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Image.asset('images/dice$numeroDado.png'),
+        Row(
+          children: [
+            FloatingActionButton(
+              onPressed: () => cambiarDado(1),
+              child: Text(
+                '1',
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.black,
+            ),
+            FloatingActionButton(
+              onPressed: () => cambiarDado(2),
+              child: Text(
+                '2',
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.black,
+            ),
+            FloatingActionButton(
+              onPressed: () => cambiarDado(3),
+              child: Text(
+                '3',
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.black,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  cambiarDado(int numero) {
+    print("El numero apretado es: $numero");
+    numeroDado = numero;
+    setState(() {});
   }
 }
